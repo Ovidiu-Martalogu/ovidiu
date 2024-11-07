73b606dff2ab8791523c0877623bd99ba158db81
Mariana M <marianarmarica@gmail.com>
2024-11-07 21:19:38 +0200

    Reformatted code for header and footer

--- ovidiu/resources/views/partials/general/footer.blade.php
+++ ovidiu/resources/views/partials/general/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer pt-0 mt-2">
-            <div class="w-100">
-                &copy;<script>document.write(new Date().getFullYear())</script>
-                Octavian Company. All rights reserved.
-            </div>
-        </footer>
+    <div class="w-100">
+        &copy;<script>document.write(new Date().getFullYear())</script>
+        Octavian Company. All rights reserved.
+    </div>
+</footer>
